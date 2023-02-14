8008e535a2374d05ae9c661081d8c7e4a1d043f4
MuhammedShahin <mohamedshahin796@gmail.com>
2023-02-14 20:42:49 +0200

    configure nvim-tree

---
+++ after/plugin/nvim-tree.lua
@@ -0,0 +1,26 @@
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup({
+    renderer = {
+       icons = {
+            show = {
+                file = false,
+                folder = false,
+                folder_arrow = false,
+                git = false,
+                modified = false,
+            },
+        }
+    }
+})
+
+local function open_nvim_tree()
+    require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
